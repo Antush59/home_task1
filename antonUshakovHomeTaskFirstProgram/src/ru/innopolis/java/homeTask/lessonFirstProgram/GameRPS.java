6777dbebff3be9dd3d9418e1200f8b73b7e3c89e
Anton Ushakov <anton-dank@yandex.ru>
2024-05-31 20:21:37 +0300

    Исправление ДЗ

--- antonUshakovHomeTaskFirstProgram/src/ru/innopolis/java/homeTask/lessonFirstProgram/GameRPS.java
+++ antonUshakovHomeTaskFirstProgram/src/ru/innopolis/java/homeTask/lessonFirstProgram/GameRPS.java
@@ -1,7 +1,5 @@
 package ru.innopolis.java.homeTask.lessonFirstProgram;
 
-import org.w3c.dom.ls.LSOutput;
-
 import java.util.Random;
 
 /**
--- antonUshakovHomeTaskFirstProgram/src/ru/innopolis/java/homeTask/lessonFirstProgram/OutputOfNumbers.java
+++ antonUshakovHomeTaskFirstProgram/src/ru/innopolis/java/homeTask/lessonFirstProgram/OutputOfNumbers.java
@@ -11,8 +11,8 @@ import java.util.Random;
 public class OutputOfNumbers {
     public static void main(String[] args) {
         Random rd = new Random();
-        System.out.println(rd.nextInt(100) + " " +
-                rd.nextInt(100) + " " + rd.nextInt(100));
+        System.out.println(rd.nextInt(100) + "   " + rd.nextInt(100)
+                + "   " + rd.nextInt(100) +  "   " + rd.nextInt(100));
     }
 }
 
