46dd6066a647ee4ef80e53620eea49c738c52df3
Anton Ushakov <anton-dank@yandex.ru>
2024-07-24 10:35:12 +0300

    "homeworks/homework10Addition"
    Изучен класс Optional. Создан похожий на него класс Pair и созданы переменные, методы и конструктор согласно условиям задачи.
    в классе PairExample проведена проверка.
    Внес проверку создания объекта выводом в консоль

--- antonUshakovHomeTaskFirstProgram/src/ru/innopolis/java/homeworks/homework10Addition/PairExample.java
+++ antonUshakovHomeTaskFirstProgram/src/ru/innopolis/java/homeworks/homework10Addition/PairExample.java
@@ -6,6 +6,9 @@ public class PairExample {
         Integer i = pair.getFirst(); // 1
         String s = pair.getSecond(); // "hello"
 
+        System.out.println(i);
+        System.out.println(s);
+
         Pair<Integer, String> pair2 = Pair.of(1, "hello");
         boolean mustBeTrue = pair.equals(pair2); // true!
         boolean mustAlsoBeTrue = pair.hashCode() == pair2.hashCode(); // true!
