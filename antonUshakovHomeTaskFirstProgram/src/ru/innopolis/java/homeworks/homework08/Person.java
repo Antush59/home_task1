8dc1f5725e0a9a11b2e87d54dbe1d9a2448169c7
Anton Ushakov <anton-dank@yandex.ru>
2024-07-28 21:02:02 +0300

    "homeworks/homework08Addition"
    Подправил код в соответствии с homework06

--- antonUshakovHomeTaskFirstProgram/src/ru/innopolis/java/homeworks/homework08/Person.java
+++ antonUshakovHomeTaskFirstProgram/src/ru/innopolis/java/homeworks/homework08/Person.java
@@ -3,22 +3,23 @@ package ru.innopolis.java.homeworks.homework08;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Person {
 
     private final String name;
     private Integer amountOfMoney;
-    private List<String> productPackage = new ArrayList<>();
+    private List<Product> productPackage = new ArrayList<>();
 
-    public Person(String name, Integer amountOfMoney, List<String> productPackage) {
+    public Person(String name, Integer amountOfMoney, List<Product> productPackage) {
         this.name = name;
         this.amountOfMoney = amountOfMoney;
         this.productPackage = productPackage;
     }
 
     public Person(String[] value) {
-            name = value[0];
-            amountOfMoney = Integer.parseInt(value[1]);
+        name = value[0];
+        amountOfMoney = Integer.parseInt(value[1]);
     }
 
     public String getName() {
@@ -29,37 +30,49 @@ public class Person {
         return amountOfMoney;
     }
 
-    public void setAmountOfMoney(Integer amountOfMoney) {
+    private void setAmountOfMoney(Integer amountOfMoney) {
         this.amountOfMoney = amountOfMoney;
     }
 
-    public List<String> getProductPackage() {
+    public List<Product> getProductPackage() {
         return productPackage;
     }
 
-    public void addProductToPacket(String productName) {
-        productPackage.add(productName);
+    public boolean addProductToPackage(Product product) {
+        if (getAmountOfMoney() < product.getPrice()) {
+            return false;
+        } else {
+            setAmountOfMoney(getAmountOfMoney() - product.getPrice());
+            productPackage.add(product);
+            return true;
+        }
+    }
+
+    private String getProductNameOfProductPackage(List<Product> products) {
+        if (products.isEmpty()) {
+            return "Ничего не куплено!";
+        } else {
+            return products.stream()
+                    .map(Product::getProductName)
+                    .collect(Collectors.joining(", "));
+        }
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Person person)) return false;
-        return Objects.equals(getName(), person.getName()) && Objects.equals(getAmountOfMoney(),
-                person.getAmountOfMoney()) && Objects.equals(getProductPackage(), person.getProductPackage());
+        return Objects.equals(name, person.name) && Objects.equals(amountOfMoney, person.amountOfMoney)
+                && Objects.equals(productPackage, person.productPackage);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getName(), getAmountOfMoney(), getProductPackage());
+        return Objects.hash(name, amountOfMoney, productPackage);
     }
 
     @Override
     public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", amountOfMoney=" + amountOfMoney +
-                ", productPackage=" + productPackage +
-                '}';
+        return getName() + " - " + getProductNameOfProductPackage(productPackage);
     }
 }
